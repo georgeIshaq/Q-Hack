6a151258d3f553650605cf2d49f62fab528fa501
george <george@ishaq.com>
2024-04-04 11:24:15 +0200

    Implemented chat functionality

--- Linking.js
+++ Linking.js
@@ -16,8 +16,38 @@ function submitForm() {
     body: JSON.stringify({"user_input": userInput})
   })
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    var resDiv = document.createElement('div');
+    resDiv.innerHTML = `<div class="uui-testimonial14_client">
+                        <div class="uui-testimonial14_client-image-wrapper"><img src="images/Chara_1.webp" loading="lazy" alt="" sizes="48px" srcset="images/Chara_1Chara.webp 500w, images/Chara_1.webp 798w" class="uui-testimonial14_customer-image"></div>
+                        <div class="uui-testimonial14_client-info">
+                        <div class="uui-testimonial14_client-heading">Mathy Co</div>
+                        <div class="uui-text-size-small-2">${data.response}</div>
+                        </div>
+                    </div>`;
+    var block = document.getElementsByClassName('chatblock')
+    block[0].appendChild(resDiv);
+})
   .catch((error) => {
     console.error('Error:', error);
   });
+
+    // Create a new div element
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute('id', 'Please God work')
+    console.log('Data:', userInput)
+    newDiv.innerHTML = `
+                        <div class="uui-testimonial14_client">
+                        <div class="uui-testimonial14_client-image-wrapper"><img src="images/Chara2_1.webp" loading="lazy" alt="" sizes="48px" srcset="images/Chara2_1Chara2.webp 500w, images/Chara2_1Chara2.webp 800w, images/Chara2_1.webp 859w" class="uui-testimonial14_customer-image"></div>
+                        <div class="uui-testimonial14_client-info">
+                            <div class="uui-testimonial14_client-heading">George</div>
+                            <div class="uui-text-size-small-2">${userInput}</div>
+                        </div>
+                        </div>`;
+    
+
+    // Set the text received as the content of the new div
+    // Append the new div to the document body
+    var block = document.getElementsByClassName('chatblock')
+    block[0].appendChild(newDiv);
 }
